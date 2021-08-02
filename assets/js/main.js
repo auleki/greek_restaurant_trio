7f8f9fd900fc06c7591c7b7412478b605db8f348
auleki <kodagiwa@gmail.com>
2021-08-02 10:16:20 +0100

    completed footer, loader, order section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector('#menu-bar')
 const navbar = document.querySelector('.navbar')
+const scrollTop = document.querySelector('#scroll-top')
+const loaderContainer = document.querySelector('.loader_container')
 
 menu.onclick = () => {
   menu.classList.toggle('fa-times')
@@ -9,4 +11,20 @@ menu.onclick = () => {
 window.onscroll = () => {
   menu.classList.remove('fa-times')
   navbar.classList.remove('active')
+
+  if (window.scrollY > 60) {
+    scrollTop.classList.add('active')
+  } else {
+    scrollTop.classList.remove('active')
+  }
 }
+
+function loader () {
+  loaderContainer.classList.add('fade-out')
+}
+
+function fadeOut () {
+  setInterval(loader, 3000)
+}
+
+window.onload = fadeOut()
